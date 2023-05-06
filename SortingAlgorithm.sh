9297ac292393239f615447ae7aed49c92fe1c4a1
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-06 13:49:56 +0530

    Add: Sort the results toshow theComputation Resultin the DescendingOrder

--- SortingAlgorithm.sh
+++ SortingAlgorithm.sh
@@ -1,18 +1,23 @@
 echo "Welcome to Sorting Algorithm Problems"
 declare -A results
+
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
+
 results["a+b*c"]=$((a + b * c))
+
+
 results["a*b+c"]=$((a * b + c))
-
 results["c+a/b"]=$(bc -l <<< "$c + $a / $b")
-
 results["a%b+c"]=$((a % b + c))
 values=("${results[@]}")
 
-echo "Values:"
-for value in "${values[@]}"; do
+sorted_values=($(for value in "${values[@]}"; do echo "$value"; done | sort -rn))
+
+echo "Sorted Values (Descending Order):"
+for value in "${sorted_values[@]}"; do
   echo "$value"
 done
+
