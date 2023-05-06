02780d31db3b02cc2de397f1921f16557781c7ba
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-06 13:14:18 +0530

    Add: sorting Algorithm to Computec+a/b)

--- SortingAlgorithm.sh
+++ SortingAlgorithm.sh
@@ -1,12 +1,7 @@
-part_time_hour=8
-
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
-result=$((a * b + c))
+result=$(bc -l <<< "$c + $a / $b")
 
-echo "Result: $result"
-part_time_result=$((part_time_hour * result))
-echo "Part-time result: $part_time_result"
-
+echo "Result: $result
