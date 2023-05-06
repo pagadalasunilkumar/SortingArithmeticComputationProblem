e8b50335e1f3c81bd90658fb715162813de16e06
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-06 12:51:22 +0530

    Add: Compute a + b * c

--- SortingAlgorithm.sh
+++ SortingAlgorithm.sh
@@ -2,6 +2,5 @@ read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
-echo "a = $a"
-echo "b = $b"
-echo "c = $c"
+result=$((a + b * c))
+echo "Result: $result"
