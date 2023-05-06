a7b3654c040d42ab710c2229890d2bd70e9341c7
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-06 13:33:11 +0530

    Add: Store the results ina Dictionary forevery Computation

--- SortingAlgorithm.sh
+++ SortingAlgorithm.sh
@@ -1,8 +1,22 @@
-echo "Welcome to sorting algorithm problems"
+echo "Welcome to Sorting Algorithm Problems"
+declare -A results
+
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
-result=$((a % b + c))
+results["a+b*c"]=$((a + b * c))
+results["a*b+c"]=$((a * b + c))
+results["c+a/b"]=$(bc -l <<< "$c + $a / $b")
+results["a%b+c"]=$((a % b + c))
 
-echo "Result: $result"
+echo "Results:"
+for key in "${!results[@]}"; do
+  echo "$key = ${results[$key]}"
+done
+
+
+
+
+
+
