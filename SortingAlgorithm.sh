7cb8e727a227ed4138219596ec15a2f8d54b51b2
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-06 13:55:31 +0530

    Add: Sort the results toshow theComputation Resultin the DescendingOrder

--- SortingAlgorithm.sh
+++ SortingAlgorithm.sh
@@ -5,13 +5,14 @@ read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
-
 results["a+b*c"]=$((a + b * c))
 
-
 results["a*b+c"]=$((a * b + c))
+
 results["c+a/b"]=$(bc -l <<< "$c + $a / $b")
+
 results["a%b+c"]=$((a % b + c))
+
 values=("${results[@]}")
 
 sorted_values=($(for value in "${values[@]}"; do echo "$value"; done | sort -rn))
