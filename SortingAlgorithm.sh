71afa1e8bfbd9a0f5240e4eaeb09aeba40921ceb
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-06 13:43:47 +0530

    Add:  valuesfrom the Dictionaryinto the array

--- SortingAlgorithm.sh
+++ SortingAlgorithm.sh
@@ -1,22 +1,18 @@
 echo "Welcome to Sorting Algorithm Problems"
 declare -A results
-
 read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
 results["a+b*c"]=$((a + b * c))
 results["a*b+c"]=$((a * b + c))
+
 results["c+a/b"]=$(bc -l <<< "$c + $a / $b")
+
 results["a%b+c"]=$((a % b + c))
+values=("${results[@]}")
 
-echo "Results:"
-for key in "${!results[@]}"; do
-  echo "$key = ${results[$key]}"
+echo "Values:"
+for value in "${values[@]}"; do
+  echo "$value"
 done
-
-
-
-
-
-
